19a88f33ab0bb57a32e33da52b97f85d78834ab5
Filippo Gasparini <filippogaspa02@gmail.com>
2024-11-19 15:14:06 +0100

    ADD: correzione

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 console.log('posts')
+
+const app = express()
+const port = 3000
+
+
+app.use(express('static'))
+
+const posts = require('posts')
+console.log(posts)
+
+router.get('routers/posts.js', (req,res) => {
+    console.log(res)
+})
